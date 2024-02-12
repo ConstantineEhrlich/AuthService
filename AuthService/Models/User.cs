2f62fc6980acaa7bffd8d740880a310005e64a05
constantine <ferrumresistance@gmail.com>
2024-02-12 23:01:03 +0200

    Code cleanup

--- AuthService/Models/User.cs
+++ AuthService/Models/User.cs
@@ -4,12 +4,11 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace AuthService.Models;
 
 public class User
-{   
-    [BsonId]
-    public string Login { get; set; }
-    public string Email { get; set; }
+{
+    [BsonId] public string Login { get; set; } = null!;
+    public string Email { get; set; } = null!;
     
-    public string PasswordHash { get; set; }
+    public string PasswordHash { get; set; } = null!;
     public DateTime? LastSuccessLogin { get; set; }
     public DateTime? LastFailedLogin { get; set; }
     public int FailedLoginCount { get; set; }
--- AuthService/Services/DTO/SignIn.cs
+++ AuthService/Services/DTO/SignIn.cs
@@ -4,6 +4,6 @@ namespace AuthService.Services.DTO;
 
 public class SignIn
 {
-    [Required] public string Login { get; set; }
-    [Required] public string Password { get; set; }
+    [Required] public string Login { get; set; } = null!;
+    [Required] public string Password { get; set; } = null!;
 }
